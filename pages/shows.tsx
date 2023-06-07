64740f3ced4dea56b9966827311ec5853e2e1c97
Daniel Sockness <danielsock@gmail.com>
2023-06-07 17:23:32 -0500

    Fixed naming convention for shows map

--- pages/shows.tsx
+++ pages/shows.tsx
@@ -46,17 +46,17 @@ export default function Movies() {
 					<>
 						{data.pages.map((page, i) => (
 							<React.Fragment key={i}>
-								{page.results.map((movie) => (
+								{page.results.map((show) => (
 									<div
 										className="flex flex-col items-center justify-center w-[400px] h-[650px] xs:w-[250px]
                                     xs:h-[375px] rounded bg-gray-50 bg-cover dark:bg-gray-800"
 										style={{
-											backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movie.poster_path})`,
+											backgroundImage: `url(https://image.tmdb.org/t/p/w500/${show.poster_path})`,
 										}}
 									>
 										<div className="opacity-0 hover:opacity-100 hover:backdrop-blur-sm duration-300 flex flex-col items-center justify-center h-full w-full">
 											<span className=" text-2xl text-gray-400 dark:text-white mb-5">
-												{movie.original_title}
+												{show.name}
 											</span>
 											<button className="inline-block rounded border-2 border-primary-100 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:border-primary-accent-100 hover:bg-neutral-500 hover:bg-opacity-10 focus:border-primary-accent-100 focus:outline-none focus:ring-0 active:border-primary-accent-200 dark:text-primary-100 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10">
 												View
