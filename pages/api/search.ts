45612adf385f4af5473c9a7d1a07440d74a6284f
Daniel Sockness <danielsock@gmail.com>
2023-06-07 20:54:34 -0500

    Search Layout, API for searching

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,59 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
-let tempData = {
-	variant: "rapidapi_basic",
-	results: [
-		{
-			locations: [
-				{
-					display_name: "Netflix",
-					id: "5d84d6e2d95dc7385f6a442d",
-					url: "https://www.netflix.com/title/70298933",
-					name: "NetflixIVAGB",
-					icon: "https://utellyassets7.imgix.net/locations_icons/utelly/black_new/NetflixIVAGB.png?w=92&auto=compress&app_version=23f8e481-9672-42ce-b1c8-b950bb12d45a_e12122021-01-29",
-				},
-			],
-			weight: 5653,
-			id: "5d914028302b840050acbe62",
-			external_ids: {
-				imdb: {
-					url: "https://www.imdb.com/title/tt3398228",
-					id: "tt3398228",
-				},
-				tmdb: {
-					url: "https://www.themoviedb.org/tv/61222",
-					id: "61222",
-				},
-				iva: {
-					id: "783721",
-				},
-				facebook: null,
-				rotten_tomatoes: null,
-				wiki_data: {
-					url: "https://www.wikidata.org/wiki/Q17733404",
-					id: "Q17733404",
-				},
-				iva_rating: null,
-				gracenote: null,
-			},
-			picture:
-				"https://utellyassets9-1.imgix.net/api/Images/4e4d50a0040fd4500193202edbafce6a/Redirect?fit=crop&auto=compress&crop=faces,top",
-			provider: "iva",
-			name: "BoJack Horseman",
-		},
-	],
-	updated: "2021-01-29T05:09:14+0000",
-	term: "bojack",
-	status_code: 200,
-};
-
+import axios from "axios";
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
 	if (req.method === "POST") {
-		console.log(req.body.search);
-		res.json(tempData);
+		var data;
+		if (req.body.type === "movie") {
+			const response = await axios.get(
+				`https://api.themoviedb.org/3/search/movie?language=en-US&include_adult=false&page=1&api_key=${process.env.TMDB_API_KEY}&query=${req.body.search}}`
+			);
+			data = response.data;
+		} else if (req.body.type === "show") {
+			const response = await axios.get(
+				`https://api.themoviedb.org/3/search/tv?language=en-US&include_adult=false&page=${req.query.page}&api_key=${process.env.TMDB_API_KEY}&query=${req.body.search}`
+			);
+			data = response.data;
+		} else {
+		}
+		res.json(data);
 	} else {
 		res.status(405).send("Method not allowed");
 	}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,91 @@
 import { Label, TextInput, Select, Button } from "flowbite-react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useInView } from "react-intersection-observer";
+import { useInfiniteQuery, useQueryClient } from "react-query";
 export default function Home() {
+	const queryClient = useQueryClient();
+	const [title, setTitle] = useState("");
+	const [type, setType] = useState("movie");
+	const handleSubmit = async () => {
+		console.log("Clicked submitting search");
+		var response = await axios.post("/api/search", {
+			search: title,
+			type: type,
+		});
+		console.log(response.data);
+	};
+
+	const { ref, inView } = useInView();
+
+	const {
+		status,
+		data,
+		error,
+		isFetching,
+		isFetchingNextPage,
+		isFetchingPreviousPage,
+		fetchNextPage,
+		fetchPreviousPage,
+		hasNextPage,
+		hasPreviousPage,
+	} = useInfiniteQuery(
+		["search"],
+		async ({ pageParam = 1 }) => {
+			const res = await axios.post("/api/search", {
+				search: title,
+				type: type,
+				page: pageParam,
+			});
+			return res.data;
+		},
+		{
+			getNextPageParam: (lastPage) => {
+				return lastPage.page < lastPage.total_pages // Here I'm assuming you have access to the total number of pages
+					? lastPage.page + 1
+					: undefined; // If there is not a next page, getNextPageParam will return undefined and the hasNextPage boolean will be set to 'false'
+			},
+		}
+	);
+	useEffect(() => {
+		if (inView) {
+			fetchNextPage();
+		}
+	}, [inView]);
+
 	return (
 		<div className="p-4">
 			<div className="p-4 rounded-lg dark:border-gray-700">
 				<div className="flex items-center justify-center mb-4 rounded bg-gray-50 dark:bg-gray-800">
-					<form className="flex flex-col gap-4 w-5/6 mb-2">
+					<div className="flex flex-col gap-4 w-5/6 mb-2">
 						<div>
 							<div className="mb-2 block">
 								<Label htmlFor="name" value="Movie/Show Name" />
 							</div>
-							<TextInput id="name" required type="text" />
+							<TextInput
+								id="name"
+								required
+								type="text"
+								value={title}
+								onChange={(e) => {
+									setTitle(e.currentTarget.value);
+									queryClient.resetQueries();
+								}}
+							/>
 						</div>
-						<Select id="type" name="type" required>
-							<option value="any">Any</option>
+						<Select
+							id="type"
+							name="type"
+							required
+							onChange={(e) => setType(e.currentTarget.value)}
+						>
 							<option value="movie">Movie</option>
 							<option value="series">Show</option>
 						</Select>
-						<Button type="submit">Search</Button>
-					</form>
+						<Button type="submit" onClick={handleSubmit}>
+							Search
+						</Button>
+					</div>
 				</div>
 				<div className="grid grid-cols-4 gap-4 mb-4">
 					<div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
@@ -35,6 +102,17 @@ export default function Home() {
 					</div>
 				</div>
 			</div>
+			<button
+				ref={ref}
+				onClick={() => fetchNextPage()}
+				disabled={!hasNextPage || isFetchingNextPage}
+			>
+				{isFetchingNextPage
+					? "Loading more..."
+					: hasNextPage
+					? "Load Newer"
+					: "Nothing more to load"}
+			</button>
 		</div>
 	);
 }
